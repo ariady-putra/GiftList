19a2e22f053968c85082dfdba6b7e4e49abd5c25
Ariady Putra <ariady.putra@gmail.com>
2026-03-27 18:44:35 +0700

    The answer is litterally there in the `utils` Thank God

--- client/index.js
+++ client/index.js
@@ -3,15 +3,15 @@ const niceList = require('../utils/niceList.json');
 const MerkleTree = require('../utils/MerkleTree');
 
 const serverUrl = 'http://localhost:1225';
+const merkleTree = new MerkleTree(niceList);
 
-async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+async function main(name) {
+  const index = niceList.findIndex(n => n === name);
+  const proof = merkleTree.getProof(index);
 
-  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
-  });
+  const { data: gift } = await axios.post(`${serverUrl}/gift`, { name, proof });
 
   console.log({ gift });
 }
 
-main();
+main(process.argv[2]);
